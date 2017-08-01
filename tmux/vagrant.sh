ec2d2d97f8cb53ebf8267d3001dfcf64b5248f2f
Owen Lin <owen.lin@airbnb.com>
2017-08-01 13:07:02 -0700

    update

--- tmux/vagrant.sh
+++ tmux/vagrant.sh
@@ -13,18 +13,16 @@ tmux new-session -s $SESSION_NAME -d
 # first pane
 tmux rename-window monorail
 tmux send-keys -t $SESSION_NAME 'cd ~/repos/airbnb && airlab ssh' C-m
-tmux send-keys -t $SESSION_NAME 'cd repos/airbnb && bundle install && sudo service monorail restart && sudo service nginx restart || rm .zeus.sock && zeus start' C-m
+tmux send-keys -t $SESSION_NAME 'bundle install && sudo service monorail restart && sudo service nginx restart || rm .zeus.sock && zeus start' C-m
 
 # second pane
 tmux split-window -h -t $SESSION_NAME
 tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 8 ) + 1 ]s; cd ~/repos/airbnb && airlab ssh' C-m
-tmux send-keys -t $SESSION_NAME 'cd repos/airbnb' C-m
 
 # third pane
 tmux select-pane -L
 tmux split-window -v -t $SESSION_NAME
 tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 8 ) + 1 ]s; cd ~/repos/airbnb && airlab ssh' C-m
-tmux send-keys -t $SESSION_NAME 'cd repos/airbnb' C-m
 
 ### Open window
 tmux new-window
